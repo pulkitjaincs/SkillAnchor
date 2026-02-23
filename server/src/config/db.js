ee48ecfe7f2da68a05fe8ec6907ec4d32735abfd
Pulkit Jain <pulkitjain.cse@gmail.com>
2026-02-23 10:05:20 +0530

    Updated the db.js to console.log SkillAnchorDB Connection

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 const connectDB = async()=>{
     try{
         await mongoose.connect(process.env.MONGO_URI);
-        console.log("MongoDB Connected with KaamSetuDB");
+        console.log("MongoDB Connected with SkillAnchorDB");
     }catch(error){
         console.log(error);
         process.exit(1);
